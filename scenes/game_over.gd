8553cadcbbf57469a0426d329e9bf96d0d23c1ec
Simon Smulders <simsmulders@gmail.com>
2026-05-29 13:37:21 +0200

    main: added high score to game over screen

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -4,6 +4,7 @@ extends Control
 
 func _ready() -> void:
 	$CenterContainer/VBoxContainer/Label2.text = $CenterContainer/VBoxContainer/Label2.text + str(Global.score)
+	$CenterContainer/VBoxContainer/MarginContainer/Label3.text = $CenterContainer/VBoxContainer/MarginContainer/Label3.text + str(Global.high_score)
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("restart"):
